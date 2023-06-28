042555a4efb4804398e754ef94345591288432bb
LKorso <skrypnykdr@gmail.com>
2023-06-28 20:39:13 +0200

    Set up multi-module project  - Change `.gitignore` to include entire `.idea/` folder;  - Add java-conventions Gradle plugin to have common java Gradle set-up in one place;  - Add simple Demo class and test to check if set-up works as expected.

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("dbs-learning.java-convention")
+}
+
+group = "org.dbs"
+version = "1.0-SNAPSHOT"
---
+++ src/main/java/org/dbs/Demo.java
@@ -0,0 +1,8 @@
+package org.dbs;
+
+public class Demo {
+
+    public String helloWorld() {
+        return "Hello world!";
+    }
+}
